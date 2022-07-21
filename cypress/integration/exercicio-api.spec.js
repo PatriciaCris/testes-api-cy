9e9001df6ae6d790b8b3c5ab1121a4010b2d2c26
Patricia Cristina <patcrisaragao@gmail.com>
2022-07-20 20:01:52 -0400

    Correções solicitadas

--- cypress/integration/exercicio-api.spec.js
+++ cypress/integration/exercicio-api.spec.js
@@ -24,37 +24,29 @@ describe('Testes da Funcionalidade Usuários', () => {
 
     it('Deve cadastrar um usuário com sucesso', () => {
           email = `email@${Math.floor(Math.random() * 100000)}.com`
-          cy.request({
-          method: 'POST',
-          url: 'usuarios',
-          body:{
-                    "nome": "Patricia",
-                    "email": email,
-                    "password": "teste",
-                    "administrador": "true"
-               }
-          }).then((response) => {
+          cy.cadastrarUsuario("Patricia", email, "teste123", "true")
+          .then((response) => {
                expect(response.status).to.equal(201)
                expect(response.body.message).to.equal('Cadastro realizado com sucesso')
           }) 
     });
 
     it('Deve validar um usuário com email inválido', () => {
-          cy.request({
-          method: 'POST',
-          url: 'usuarios',
-          body:{
-                    "nome": "Patricia",
-                    "email": email,
-                    "password": "teste",
-                    "administrador": "true"
-               },
-               failOnStatusCode: false
-          }).then((response) => {
+          cy.cadastrarUsuario("Patricia", email, "testando", "true")
+          .then((response) => {
                expect(response.status).to.equal(400)
                expect(response.body.message).to.equal('Este email já está sendo usado')
           }) 
-    });
+     })
+
+     it('Deve validar um usuário com email - formato inválido', () => {
+          email = "patricia.com"
+          cy.cadastrarUsuario("Patricia", email, "testando", "true")
+          .then((response) => {
+               expect(response.status).to.equal(400)
+               expect(response.body.email).to.equal('email deve ser um email válido')
+          }) 
+     })
 
     it('Deve editar um usuário previamente cadastrado', () => {
           email = `email@${Math.floor(Math.random() * 100000)}.com`
@@ -79,16 +71,7 @@ describe('Testes da Funcionalidade Usuários', () => {
 
     it('Deve deletar um usuário previamente cadastrado', () => {
           email = `email@${Math.floor(Math.random() * 100)}.com`
-          cy.request({
-               method: 'POST',
-               url: 'usuarios',
-               body:{
-                         "nome": "Patricia",
-                         "email": email,
-                         "password": "teste",
-                         "administrador": "true"
-                    }
-               })
+          cy.cadastrarUsuario("Patricia", email, "teste", "true")
           .then(response => {
           let id = response.body._id
           cy.request({
@@ -99,7 +82,5 @@ describe('Testes da Funcionalidade Usuários', () => {
                expect(response.status).to.equal(200)
           })
           })
-    });
-
-
-});
+    })
+})
